1a851bbb4283c0dc16513b8b804a11a18c4c27c1
PauloTeixeira <paulo.teixeira541@gmail.com>
2019-10-06 00:31:51 -0300

    Iniciando o javaScript básico

---
+++ scripts/aula01.js
@@ -0,0 +1,51 @@
+/**
+ * Brendan Eich - considerado o criador da linguagem.
+ * 
+ * javaScript é uma linguagem de tipagem dinâmica.
+ * 
+ * Linguagem case sensitive - diferencia letras maiúsculas de minúsculas.
+ * 
+ * obs.: Esse tipo de comentário geralmente é usado pra documentação.
+ * 
+ */
+
+// Comentário de uma linha
+
+/*
+Comentários de múltiplas linhas
+aaaaa
+bbbbb
+ccccc
+*/
+
+//Variáveis - são declaradas com o comando 'let' ou 'var'.
+let name = 'Paulo.js'; // aspas duplas ou simples.
+var age = 34; // O ponto e vírgula é opcional.
+
+//Valor constante - É declarado com o comando 'const'.
+const cpf = '000.000.000-00'; // Usa-se quando se declara um valor que não mudará.
+
+
+/* 
+Você pode encontrar a documentação do javaScript no site da MDN
+link: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript
+*/
+
+/*
+JavaScript é mantido pela ECMA.
+javaScript é a mesma coisa que ecman script
+javaScript é diferente de java.
+*/
+
+//Tipos primitivos de dados em javaScript
+/* 
+string
+number
+boolean
+null
+undefined
+symbol
+*/
+
+// Escreve no documento html.
+document.write("HelloWorld"); // Chamamos esse tipo de escrita de CamelCase.
